f4eabf8ebc5ef92d63baa403c634f36cf1936a9e
ZychGrzegorz <grzegorzzych95@gmail.com>
2021-02-01 16:37:14 +0100

    env

--- server.js
+++ server.js
@@ -5,14 +5,16 @@ const path = require('path');
 const app = express();
 
 // app.use('/js', express.static(path.resolve(__dirname, 'src', 'js')));
-app.use('/static', express.static(path.resolve(__dirname, 'src', 'static')));
-// app.use('/static', express.static(path.resolve(__dirname, 'dist', 'static')));
+// app.use('/static', express.static(path.resolve(__dirname, 'src', 'static')));
+app.use('/static', express.static(path.resolve(__dirname, 'dist', 'static')));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'src', 'index.html'));
-  // res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+  // res.sendFile(path.resolve(__dirname, 'src', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
 });
-app.listen(process.env.port||5080,()=>{console.log('server is running')})
+app.listen(process.env.port || 5080, () => {
+  console.log('server is running');
+});
 // app.listen(5080, () => {
 //   console.log('server is running');
 // });
